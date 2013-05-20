0d3f43e20c654a91d2064cc9f56f6ac97daaf350
Brad Coudriet <brad@exula.net>
2013-05-20 15:18:10 -0400

    Exludes?

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ apt-get update
 
 echo "mysql-server-5.5 mysql-server/root_password_again password root"| debconf-set-selections
 echo "mysql-server-5.5 mysql-server/root_password password root" | debconf-set-selections
-apt-get install -y apache2 php5 php5-mysql mysql-server
+apt-get install -y php5 php5-mysql mysql-server apache2 
 rm -rf /var/www
 ln -fs /vagrant/web/ /var/www
 
