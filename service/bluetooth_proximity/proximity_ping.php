fe8ec0131f365d17a68206116d94756a0355a911
Bradley Coudriet <brad@exula.net>
2013-09-23 21:06:41 -0400

    Moved from l2ping to hcitool name

--- service/bluetooth_proximity/proximity_ping.php
+++ service/bluetooth_proximity/proximity_ping.php
@@ -67,12 +67,13 @@ while(true) {
 			
 			$a = str_replace('-',':',$users['users'][$user]['bluetooth']);
 			$u = $user;
-			$cmd = "sudo l2ping -s 1 -c 2 -t 2 $a > /dev/null  ; echo $?";
+			$cmd = "sudo hcitool name $a ";
+			//$cmd = "sudo l2ping -s 1 -c 2 -t 2 $a > /dev/null  ; echo $?";
 			//echo $cmd."\n";
 			$ret = exec($cmd);
 			
-
-			if($ret == 0) {
+			if(!empty($ret)) {
+			//if($ret == 0) {
 				if(!isset($holding_array[$u]) || $holding_array[$u] == 'out') {
 					//Update the server
 					$url = $URL.'?update=true&username='.$u.'&status=in&bt='.$a.'&key='.$apikey;
